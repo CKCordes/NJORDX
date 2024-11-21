4b55d577781127377c561e2eb800d1091de38176
mkolind <madsskolind@gmail.com>
2024-11-21 15:45:51 +0000

    Fixed missing test for order

--- test/unit_tests/test_order.cpp
+++ test/unit_tests/test_order.cpp
@@ -2,25 +2,31 @@
 #include "order.hpp"
 
 TEST_CASE("Testing Order class") {
-    /*Order order(1, OrderType::BUY, 1, "AAPL", 10, 100.0);
+
+    Stock stock(1, "AAPL", 100);
+
+    Order order(1, OrderType::BUY, 1, std::make_shared<Stock>(stock), 10, 100.0);
     CHECK(order.getOrderID() == 1);
     CHECK(order.getOrderType() == OrderType::BUY);
     CHECK(order.getTraderID() == 1);
     CHECK(order.getStockSymbol() == "AAPL");
-    CHECK(order.getStockID() == -1);
+    CHECK(order.getStockID() == 1);
     CHECK(order.getQuantity() == 10);
     CHECK(order.getPrice() == 100.0);
     CHECK(order.getIsFilled() == false);
+    //CHECK(order.getTransactionFee<OrderType::BUY>() == 0.01);
 
     
-    order.setStockID(1);
-    order.setIsFilled(true);
-    CHECK(order.getStockID() == 1);
-    CHECK(order.getIsFilled() == true);
-    order.displayOrderDetails();
+    //order.setStockID(1);
+    //order.setIsFilled(true);
+    //CHECK(order.getStockID() == 1);
+    //CHECK(order.getIsFilled() == true);
+    //order.displayOrderDetails();
 
-    Order order2(2, OrderType::SELL, 2, "GOOGL", 20, 200.0);
+    Stock stock2(2, "GOOGL", 200);
+
+    Order order2(2, OrderType::SELL, 2, std::make_shared<Stock>(stock2), 20, 200.0);
     CHECK(order == order);
-    CHECK_FALSE(order == order2);*/
+    CHECK_FALSE(order == order2);
     CHECK(true);
 }
