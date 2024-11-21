b702502b097e33d37bbedb6dde054397e1e4393e
mkolind <madsskolind@gmail.com>
2024-11-21 14:42:04 +0000

    add complexity to company testing

--- test/unit_tests/test_company.cpp
+++ test/unit_tests/test_company.cpp
@@ -3,12 +3,20 @@
 #include "njordx.hpp"
 
 TEST_CASE("Testing company class"){
+    // Constructing a new exchange
+    Njordx* exchange = new Njordx();
 
-
-    Company company1(1, 100, "Company1", "12345678");
-    Company company2(2, 200, nullptr, "Company2", "87654321");
-
+    // Constructing two company1 and validating
+    Company company1(1, 100, exchange, "Company1", "12345678");
     CHECK(company1.getBalance() == 100); 
     company1.setBalance(200);
     CHECK(company1.getBalance() == 200);  
+
+    // Constructing company2 and validating    
+    Company company2(2, 200, "Company2", "87654321");
+    CHECK(company2.getBalance() == 200);
+    company2.joinExchange(exchange);
+
+    company2.createStock(1, "AAPL", 100);
+    company2.displayPortfolio();
 }
