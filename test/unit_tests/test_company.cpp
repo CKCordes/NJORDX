a1aa9f24ca7b2abcea57483cfad82cbc14a5d842
Asser <asser29@gmail.com>
2024-11-28 08:44:28 +0000

    Added tests w.r.t company. TODO: testing of order methods

--- test/unit_tests/test_company.cpp
+++ test/unit_tests/test_company.cpp
@@ -2,16 +2,30 @@
 #include "company.tpp"
 #include "njordx.hpp"
 
-TEST_CASE("Testing company class"){
+
+// Constructing two company1 and validating
+TEST_CASE("Testing initialization of company classes and getters and setters"){
     // Constructing a new exchange
     Njordx* exchange = new Njordx();
 
-    // Constructing two company1 and validating
     Company company1(1, 100, exchange, "Company1", "12345678");
     CHECK(company1.getBalance() == 100); 
     company1.setBalance(200);
     CHECK(company1.getBalance() == 200);  
+    CHECK(company1.getTraderID() == 1);
+    CHECK(company1.getCompanyName() == "Company1");
+    CHECK(company1.getRegistrationNumber() == "12345678");
+    Stock google = Stock(1, "GOOGL", 100);
+    company1.addStock(google);
+    CHECK(company1.getStock("GOOGL") == google);
+    company1.removeStock(google);
+    CHECK_THROWS(company1.getStock("GOOGL") == google);
+}
 
+TEST_CASE("Testing delegating company constructors, joinExchange() and createStock()"){
+    // Constructing a new exchange
+    Njordx* exchange = new Njordx();
+    
     // Constructing company2 and validating    
     Company company2(2, 200, "Company2", "87654321");
     CHECK(company2.getBalance() == 200);
@@ -19,4 +33,13 @@ TEST_CASE("Testing company class"){
 
     company2.createStock(1, "AAPL", 100);
     company2.displayPortfolio();
+}
+
+// TODO: lav denne test
+TEST_CASE("Testing company's order related methods"){
+    // Constructing a new exchange
+    Njordx* exchange = new Njordx();
+
+    // Constructing company3
+    Company company3(3, 300, exchange, "Company3", "123");
 }
